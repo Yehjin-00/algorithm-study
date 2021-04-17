a2f412f12f978eb7bb9384c0aba956d48817b1d7
Shin Yehjin <68529301+Yehjin-00@users.noreply.github.com>
2021-04-17 23:32:58 +0900

    Solve level 08 (20210417)

--- baekjoon_step/level-08-기본-수학-1/05-ACM-호텔-10250.py
+++ baekjoon_step/level-08-기본-수학-1/05-ACM-호텔-10250.py
@@ -1,5 +1,14 @@
 ## Title: ACM 호텔
 ## Level: 8_05
 ## Number: 10250
-## Date: 2021.04.13
+## Date: 2021.04.17
 
+import sys 
+
+num = int(input())
+
+for line in sys.stdin:
+	h, w, n = map(int, line.split())
+	room = (n-1)//h + 1
+	floor = (n-1)%h + 1
+	print(floor*100+room)
