d1e58bdb7d98403a61c51bda4fd53889be82dfcc
Shin Yehjin <68529301+yehjin-00@users.noreply.github.com>
2021-05-03 03:35:28 +0900

    Solve level 12 (3~4)

--- baekjoon_step/level-12-정렬/03-수-정렬하기-3-10989.py
+++ baekjoon_step/level-12-정렬/03-수-정렬하기-3-10989.py
@@ -1,5 +1,18 @@
 ## Title: 수 정렬하기 3
 ## Level: 12_03
 ## Number: 10989
-## Date: 2021.04.26
+## Date: 2021.05.02
 
+# 카운팅 정렬
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+count = [0] * 10000
+
+for i in range(n):
+    count[int(input())-1] += 1
+
+for i in range(10000):
+    for j in range(count[i]):
+        print(i+1)
--- baekjoon_step/level-12-정렬/04-통계학-2108.py
+++ baekjoon_step/level-12-정렬/04-통계학-2108.py
@@ -1,5 +1,31 @@
 ## Title: 통계학
 ## Level: 12_04
 ## Number: 2108
-## Date: 2021.04.26
+## Date: 2021.05.02
 
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+count = [0] * 8001
+
+for i in range(n):
+    count[int(input())+4000] += 1
+
+maxi = 0
+num = []
+check = True
+for i in range(8001):
+	if count[i] > count[maxi]:
+		maxi = i
+		check = True
+	elif count[i] == count[maxi] and check and i != maxi:
+		maxi = i
+		check = False
+	for j in range(count[i]):
+		num.append(i-4000)
+
+print(round(sum(num)/len(num)))
+print(num[len(num)//2])
+print(maxi-4000)
+print(num[-1]-num[0])
