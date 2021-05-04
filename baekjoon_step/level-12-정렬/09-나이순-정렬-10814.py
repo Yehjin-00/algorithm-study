b7a52ec8e6c806cd87a6eb395739e165570b25a9
Shin Yehjin <68529301+yehjin-00@users.noreply.github.com>
2021-05-04 14:43:52 +0900

    Solve level 12 (9~10)

--- baekjoon_step/level-12-정렬/09-나이순-정렬-10814.py
+++ baekjoon_step/level-12-정렬/09-나이순-정렬-10814.py
@@ -1,5 +1,40 @@
 ## Title: 나이순 정렬
 ## Level: 12_09
 ## Number: 10814
-## Date: 2021.04.26
+## Date: 2021.05.04
 
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+member = []
+
+for i in range(n):
+	member.append([i]+input().split())
+
+def compare(m1, m2):
+	if m1[1]==m2[1]:
+		return m1[0]-m2[0]
+	else:
+		return int(m1[1])-int(m2[1])
+
+def heapify(a, index, heap_size):
+	mini = index
+	left = 2*index + 1
+	right = 2*index + 2
+
+	if left < heap_size and compare(a[mini], a[left])>0:
+		mini = left
+	if right < heap_size and compare(a[mini], a[right])>0:
+		mini = right
+	if mini != index:
+		a[mini], a[index] = a[index], a[mini]
+		heapify(a, mini, heap_size)
+
+for i in range(len(member)//2-1, -1, -1):
+	heapify(member, i, len(member))
+
+for i in range(len(member)-1, -1, -1):
+	print(member[0][1], member[0][2])
+	member[0], member[i] = member[i], member[0]
+	heapify(member, 0, i)
--- baekjoon_step/level-12-정렬/10-좌표-압축-18870.py
+++ baekjoon_step/level-12-정렬/10-좌표-압축-18870.py
@@ -1,5 +1,37 @@
 ## Title: 좌표 압축
 ## Level: 12_10
 ## Number: 18870
-## Date: 2021.04.26
+## Date: 2021.05.04
 
+
+# 시간 초과
+num = input()
+num = list(map(int, input().split()))
+
+for i in range(len(num)):
+	num[i] = [num[i], i]
+
+num.sort()
+
+index = 0
+before = 0
+for i in range(len(num)):
+	if i==0 or num[i][0]!=before:
+		index = i
+	before = num[i][0]
+	num[i] = [num[i][1], index]
+
+num.sort()
+
+for value in num:
+	print(value[1], end=' ')
+
+
+# 재도전
+n = int(input())
+num = list(map(int, input().split()))
+ranking = sorted(list(set(num)))
+rank_dict = {ranking[i] : i for i in range(len(ranking))}
+
+for i in num:
+	print(rank_dict[i], end=' ')
