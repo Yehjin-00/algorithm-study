05048a65b86dc21db9a9a622520e7222e90bd841
Shin Yehjin <68529301+yehjin-00@users.noreply.github.com>
2022-01-23 22:24:06 +0900

    Create level 15

---
+++ baekjoon_step/level-15-동적-계획법1/01-피보나치-함수-1003.py
@@ -0,0 +1,57 @@
+## Title: 피보나치 함수
+## Level: 15_01
+## Number: 1003
+## Date: 2022.01.23
+
+# 통과
+N = int(input())
+f_dict = {0:(1,0), 1:(0,1)}
+
+def fibonacci(num, zero_lst, one_lst):
+	if num in f_dict:
+		zero_lst.append(f_dict[num][0])
+		one_lst.append(f_dict[num][1])
+	else:
+		if num-1 in f_dict:
+			zero_lst.append(f_dict[num-1][0])
+			one_lst.append(f_dict[num-1][1])
+		else:
+			zero_sum = sum(zero_lst)
+			one_sum = sum(one_lst)
+			fibonacci(num-1, zero_lst, one_lst)
+			f_dict[num-1] = (sum(zero_lst)-zero_sum, sum(one_lst)-one_sum)
+
+		if num-2 in f_dict:
+			zero_lst.append(f_dict[num-2][0])
+			one_lst.append(f_dict[num-2][1])
+		else:
+			zero_sum = sum(zero_lst)
+			one_sum = sum(one_lst)
+			fibonacci(num-2, zero_lst, one_lst)
+			f_dict[num-2] = (sum(zero_lst)-zero_sum, sum(one_lst)-one_sum)
+
+for _ in range(N):
+	n = int(input())
+	zero_lst = []
+	one_lst = []
+	fibonacci(n, zero_lst, one_lst)
+	print(sum(zero_lst), sum(one_lst))
+
+# 시간 초과
+N = int(input())
+
+def fibonacci(num, zero_lst, one_lst):
+	if num == 0:
+		zero_lst.append(1)
+	elif num == 1:
+		one_lst.append(1)
+	else:
+		fibonacci(num-2, zero_lst, one_lst)
+		fibonacci(num-1, zero_lst, one_lst)
+
+for _ in range(N):
+	n = int(input())
+	zero_lst = []
+	one_lst = []
+	fibonacci(n, zero_lst, one_lst)
+	print(sum(zero_lst), sum(one_lst))
---
+++ baekjoon_step/level-15-동적-계획법1/12-가장-긴-바이토닉-부분-수열-11054.py
@@ -0,0 +1,5 @@
+## Title: 
+## Level: 15_
+## Number: 
+## Date: 2022.01.23
+
